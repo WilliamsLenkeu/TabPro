04c06663a081300eadcd80a3ce755663530e01bc
Wally🤖 <williams25prince@gmail.com>
2024-07-31 09:21:12 +0100

    update

--- TabPro/App.xaml.cs
+++ TabPro/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Firebase.Auth;
 using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,57 +16,38 @@ namespace TabPro
     public partial class App : Application
     {
         private readonly IHost _host;
-        private FirebaseAuthProvider _authProvider;
 
         public App()
         {
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                    config.AddEnvironmentVariables();
-                })
+            _host = Host
+                .CreateDefaultBuilder()
                 .ConfigureServices((context, services) =>
                 {
                     string firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY");
-
-                    if (string.IsNullOrEmpty(firebaseApiKey))
+                    var config = new FirebaseAuthConfig
                     {
-                        throw new InvalidOperationException("FIREBASE_API_KEY is missing in the configuration.");
-                    }
+                        ApiKey = firebaseApiKey,
+                        AuthDomain = "<DOMAIN>.firebaseapp.com",
+                        Providers = new FirebaseAuthProvider[]
+                        {
+                            new EmailProvider()
+                        },
+                        UserRepository = new FileUserRepository("FirebaseSample")
+                    };
 
-                    _authProvider = new FirebaseAuthProvider(new FirebaseConfig(firebaseApiKey));
-                    services.AddSingleton(_authProvider);
-
+                    services.AddSingleton(new FirebaseAuthClient(config));
                     services.AddSingleton<MainWindow>();
                 })
                 .Build();
         }
 
-        protected override async void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
-            await _host.StartAsync();
-
             var mainWindow = _host.Services.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-
-            await CreateUserWithEmailAndPassword("test@gmail.com", "Test123!");
+            MainWindow = mainWindow;
+            MainWindow.Show();
 
             base.OnStartup(e);
-        }
-
-        protected override async void OnExit(ExitEventArgs e)
-        {
-            await _host.StopAsync();
-            _host.Dispose();
-
-            base.OnExit(e);
-        }
-
-        private async Task CreateUserWithEmailAndPassword(string email, string password)
-        {
-            var authLink = await _authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
-            Console.WriteLine($"Successfully created new user: {authLink.User.LocalId}");
         }
     }
 }
